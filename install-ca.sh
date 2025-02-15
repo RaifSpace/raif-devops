f48d1e571d06bb02a26a7ca16b78ffbdfe6904e8
Raif <ab.raif@ya.ru>
2025-02-15 19:57:13 +0300

    Update install-ca.sh

--- install-ca.sh
+++ install-ca.sh
@@ -40,3 +40,27 @@ chmod 700 ~/easy-rsa
 cd ~/easy-rsa
 ./easyrsa init-pki || exit 1
 ./easyrsa build-ca nopass || exit 1
+
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+sudo docker pull ribbybibby/ssl-exporter
+sudo docker run -d --restart=unless-stopped -p 9219:9219 ribbybibby/ssl-exporter:latest
+
+sudo apt install prometheus-node-exporter -y
+
+sudo iptables -A INPUT -p tcp --dport 9100 -s 89.169.154.113 -j ACCEPT
+sudo service netfilter-persistent save
